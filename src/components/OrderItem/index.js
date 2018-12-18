a4a905b27f52e9a2c7fa2dc966b0e6b693cbf749
xiaowan.chen <xiaowan.chen@weimob.com>
2018-12-18 14:12:14 +0800

    列表订单功能完成

--- src/components/OrderItem/index.js
+++ src/components/OrderItem/index.js
@@ -5,9 +5,53 @@ class OrderItem extends Component {
     constructor(props){
         super(props)
         this.state = {
-
+            editing: false,
+            comment: this.props.data.comment || '',
+            stars : this.props.data.stars || 0
         }
     }
+
+    handleOpenEditArea = () => {
+        this.setState({
+            editing: true
+        })
+    }
+
+    handleCommentChange = (event) => {
+        this.setState({
+            comment: event.target.value
+        })
+    }
+
+    handleClickStars = (stars) => {
+        this.setState({ stars })
+    }
+
+    handleCancleComment = () => {
+        this.setState({
+            editing: false,
+            comment: this.props.data.comment || '',
+            stars : this.props.data.stars || 0
+        })
+    }
+    
+    handleEditComment = () => {
+        this.setState({
+            editing: true,
+            comment: this.props.data.comment,
+            stars : this.props.data.stars
+        })
+    }
+
+    handleSubmitComment = () => {
+        const { id } = this.props.data;
+        const { comment, stars } = this.state;
+        this.setState({
+            editing: false, 
+        })
+        this.props.onSubmit(id, comment, stars)
+    }
+
     render() {
         const { picture, product, shop, price ,isCommented} = this.props.data;
         return (
@@ -23,17 +67,49 @@ class OrderItem extends Component {
                         <div>
                             {
                                 isCommented ? (
-                                    <button className='orderItem__btn orderItem__btn--grey'>评价</button>
+                                    <div>
+                                        <button className='orderItem__btn orderItem__btn--grey'>已评价</button>
+                                        <button className='orderItem__btn orderItem__btn--edit' onClick={ this.handleEditComment}>查看</button>
+                                    </div>
                                 ) : (
-                                    <button className='orderItem__btn orderItem__btn--red'>评价</button>
+                                    <button className='orderItem__btn orderItem__btn--red' onClick = {this.handleOpenEditArea} >评价</button>
                                 )
                             }
                             
                         </div>
                     </div>
                 </div>
+                { this.state.editing ? this.renderEditArea() : null }
             </div>
         );
+    }
+
+    renderEditArea(){
+        return (
+            <div className='orderItem__commentContainer'>
+                <textarea className='orderItem__comment' value= { this.state.comment}  onChange = { this.handleCommentChange} />
+                { this.renderStarts() }
+                <button className='orderItem__btn orderItem__btn--red' onClick = { this.handleSubmitComment}>提交</button>
+                <button className='orderItem__btn orderItem__btn--grey' onClick = { this.handleCancleComment }>取消</button>
+            </div>
+        )
+    }
+
+    renderStarts(){
+        const { stars } = this.state;
+        return (
+            <div>
+                {
+                    [1, 2, 3, 4, 5].map( (item, index) => {
+                        return (
+                            stars >= item
+                             ? (<span key= {index} role='img' aria-label='star' className='orderItem__stars' onClick = { this.handleClickStars.bind(this, item) } >⭐️</span>)
+                             : (<span key= {index} role='img' aria-label='star' className='orderItem__stars' onClick = { this.handleClickStars.bind(this, item) }>★</span>)
+                        )
+                    })
+                }
+            </div>
+        )
     }
 }
 
--- src/components/OrderList/index.js
+++ src/components/OrderList/index.js
@@ -4,16 +4,31 @@ class OrderList extends Component {
     constructor(props){
         super(props)
         this.state = {
-            data: []
+            data: [],
+            isCommentShow: false
         };
         
     }
+    handleSubmit = (id, comment, stars) => {
+        const newData = this.state.data.map( item => {
+            return item.id === id ? {
+                ...item, 
+                comment,
+                stars,
+                isCommented: true
+            } : item
+        })
+        this.setState({
+            data: newData
+        })
+    }
+
     render() {
         return (
             <div>
                 {
                     this.state.data.map( (item) => (
-                        <OrderItem key = {item.id} data = {item} />
+                        <OrderItem key = {item.id} data = {item} onSubmit = {this.handleSubmit}  />
                     ))
                 }
             </div>
